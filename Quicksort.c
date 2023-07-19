78d52d964034097dc323ae4a1d2f481a49cf6998
Tanisha Gotadke <117576461+tan-codes22@users.noreply.github.com>
2023-07-19 15:17:46 +0530

    Create Quicksort.c

---
+++ Quicksort.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+int n;
+
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+
+int partition(int array[], int low, int high) {
+ 
+
+  int pivot = array[high];
+ 
+
+  int i = (low - 1);
+
+
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+       
+   
+      i++;
+     
+     
+      swap(&array[i], &array[j]);
+    }
+  }
+
+ 
+  swap(&array[i + 1], &array[high]);
+ 
+
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+   
+
+    int pi = partition(array, low, high);
+
+    quickSort(array, low, pi - 1);
+   
+   
+    quickSort(array, pi + 1, high);
+  }
+}
+
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+
+int main() {
+  int data[20],i;
+    printf("Enter the number of elements:\n");
+  scanf("%d",&n);
+  printf("Enter the elements:\n");
+  for(i=0;i<n;i++)
+  scanf("%d",&data[i]);
+ 
+ 
+ 
+  quickSort(data, 0, n - 1);
+ 
+  printf("Sorted array: \n");
+  printArray(data, n);
+}
